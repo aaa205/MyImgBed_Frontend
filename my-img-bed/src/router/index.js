9c6867ccce5920a38aa9702765062314062dbf25
LinksRay <963030205@qq.com>
2020-07-27 11:53:02 +0800

    试图修复页头页尾样式问题

--- my-img-bed/src/router/index.js
+++ my-img-bed/src/router/index.js
@@ -1,10 +1,11 @@
+import ElementUI from 'element-ui'
 import Vue from 'vue'
 import Router from 'vue-router'
 import Main from '../components/index/Main'
 import Global from '../components/user/Global'
 import Album from '../components/user/Album'
 import Detail from '../components/user/Detail'
-import ElementUI from 'element-ui'
+
 import VueClipboard from 'vue-clipboard2'
 // import Clipboard from 'clipboard'
 import 'element-ui/lib/theme-chalk/index.css'
